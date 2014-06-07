157836230d9f06079992cec4e74ed54be5578aba
Matt Kocubinski <mkocubinski@gmail.com>
2014-06-07 16:49:56 -0400

    Web-mode, clean-up.

---
+++ init-packages.el
@@ -0,0 +1,29 @@
+(setq my-packages
+    (append '(cl-lib 
+	       evil 
+	       color-theme 
+	       solarized-theme
+	       yascroll
+	       auto-complete
+	       pos-tip
+	       helm
+	       popup
+	       yasnippet
+	       yasnippet-snippets
+	       flycheck
+	       powerline
+	       paredit
+	       rainbow-delimiters
+	       projectile
+	       js2-mode
+	       skewer-mode
+	       simple-httpd
+	       tern
+	       clojure-mode
+	       cider
+	       ac-nrepl
+	       web-mode
+	       rainbow-mode
+	       org-mode
+	      )))
+
--- init.el
+++ init.el
@@ -18,12 +18,8 @@
 
 (add-to-list 'el-get-recipe-path "~/.emacs.d/recipes")
 
-;; packages
-(setq my-packages
-    (append '(cl-lib evil color-theme solarized-theme yascroll auto-complete pos-tip helm
-		     popup yasnippet yasnippet-snippets flycheck powerline paredit
-		     rainbow-delimiters projectile js2-mode skewer-mode simple-httpd tern
-		     clojure-mode cider ac-nrepl org-mode)))
+;; load packages
+(load-file "~/.emacs.d/init-packages.el")
 
 (el-get 'sync my-packages)
 
@@ -273,6 +269,49 @@ by using nxml's indentation rules."
   (add-hook 'css-mode-hook 'skewer-css-mode)
   (add-hook 'html-mode-hook 'skewer-html-mode))
 (ome-skewer-mode-setup)
+
+;; web dev ;;
+;;;;;;;;;;;;;
+
+(defun ome-web-mode-setup ()
+  (add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.jsp\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+
+  (defun ome-web-mode-hook ()
+    ;; indentation
+    ;; HTML offset indentation
+    (setq web-mode-markup-indent-offset 2)
+    ;; CSS offset indentation
+    (setq web-mode-code-indent-offset 2)
+    ;; Script offset indentation (for JavaScript, Java, PHP, etc.)
+    (setq web-mode-css-indent-offset 2)
+    ;; HTML content indentation
+    (setq web-mode-indent-style 2)
+
+    ;; padding
+    ;; For <style> parts
+    (setq web-mode-style-padding 1)
+    ;; For <script> parts
+    (setq web-mode-script-padding 1)
+    ;; For multi-line blocks
+    (setq web-mode-block-padding 0))
+
+  (add-hook 'web-mode-hook 'ome-web-mode-hook))
+
+(ome-web-mode-setup)
+
+(require 'rainbow-mode)
+
+(defun ome-rainbow-mode-setup ()
+  (add-hook 'css-mode-hook 'rainbow-mode))
+
+(ome-rainbow-mode-setup)
 
 ;; clojure/lisp ;;
 ;;;;;;;;;;;;;;;;;;
